c4c9752138f40752df92f2719ccfe96a8f6dac09
Yulian13 <Shmigol2001@gmail.com>
2019-12-27 11:53:15 +0300

    New Prodject

---
+++ MyParserTest/MyParserTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyParserTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
